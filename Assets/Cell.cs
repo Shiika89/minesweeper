6d48b74155e8dc2a06667b1a99923386872b0267
shiika89 <kazuki_plant@yahoo.co.jp>
2021-06-02 15:39:39 +0900

    セルの周囲の検索をする内容メソッドを追加

--- Assets/Cell.cs
+++ Assets/Cell.cs
@@ -18,8 +18,19 @@ public enum CellState
 public class Cell : MonoBehaviour
 {
     [SerializeField] Text m_view = null;
-    [SerializeField] GameObject m_button;
+    [SerializeField] public GameObject m_button;
     [SerializeField] private CellState m_cellState = CellState.None;
+    GameObject mine;
+    Minesweeper minesweeper;
+    private Cell[,] cubes;
+    public Vector2Int positionCell;
+
+    private void Start()
+    {
+        mine = GameObject.Find("Minesweeper");
+        minesweeper = mine.GetComponent<Minesweeper>();
+        cubes = new Cell[minesweeper.m_indexNumX,minesweeper.m_indexNumY];
+    }
 
     public CellState CellState
     {
@@ -58,24 +69,14 @@ public class Cell : MonoBehaviour
     {
         if (CellState != CellState.Mine)
         {
-            //m_button.SetActive(false);
+            m_button.SetActive(false);
+            minesweeper.lastOpenCell = this;
+            //minesweeper.OpenCell();
+
         }
         else if (CellState == CellState.Mine)
         {
-            m_button.SetActive(false);
-        }
-    }
-
-    void searchCell(int r, int c)
-    {
-        var left = c - 1;
-        var right = c + 1;
-        var top = r - 1;
-        var bottom = r + 1;
-
-        if (top >= 0)
-        {
-            
+            //m_button.SetActive(false);
         }
     }
 }
--- Assets/Minesweeper.cs
+++ Assets/Minesweeper.cs
@@ -6,15 +6,20 @@ using UnityEngine.UI;
 public class Minesweeper : MonoBehaviour
 {
     [SerializeField] private Cell m_cellPrefab = null;
+    Cell m_cellScript;
     [SerializeField] private GridLayoutGroup m_gridLayoutGroup = null;
     [SerializeField] private int m_mineCount = 1;
-    [SerializeField] int m_indexNumX = 5;
-    [SerializeField] int m_indexNumY = 5;
+    [SerializeField] public int m_indexNumX = 5;
+    [SerializeField] public int m_indexNumY = 5;
     private Cell[,] cubes;
+    public Cell lastOpenCell;
+    
 
     // Start is called before the first frame update
     void Start()
     {
+        m_cellScript = m_cellPrefab.GetComponent<Cell>();
+
         if (m_indexNumX < m_indexNumY)
         {
             m_gridLayoutGroup.constraint = GridLayoutGroup.Constraint.FixedColumnCount;
@@ -33,6 +38,7 @@ public class Minesweeper : MonoBehaviour
             for (int x = 0; x < m_indexNumY; x++)
             {
                 var cell = Instantiate(m_cellPrefab);
+                cell.positionCell = new Vector2Int(i, x);
                 var parent = m_gridLayoutGroup.gameObject.transform;
                 cell.transform.SetParent(parent);
                 //cell.transform.position = new Vector3(-4 + i * 2, -3 + x * 2, 0);
@@ -80,6 +86,7 @@ public class Minesweeper : MonoBehaviour
         //}
     }
 
+    //Mineから見て周りのセルにカウントを足す
     private void AddMine(int r, int c)
     {
         var left = c - 1;
@@ -102,7 +109,8 @@ public class Minesweeper : MonoBehaviour
             if (right < m_indexNumY && cubes[bottom, right].CellState != CellState.Mine) { cubes[bottom, right].CellState++; }
         }
     }
-
+    
+    //周りのMineの数を数えて数値を入れる
     private int GetMineCount(int r, int c)
     {
         var left = c - 1;
@@ -128,9 +136,64 @@ public class Minesweeper : MonoBehaviour
         return count;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OpenCell()
     {
+        var x = lastOpenCell.positionCell.x;
+        var y = lastOpenCell.positionCell.y;
+
+        //m_cellScript.m_button.SetActive(false);
+
+        foreach (var cell in SearchCell(x,y))
+        {
+            if (cell.CellState == CellState.None)
+            {
+                OpenCell();
+            }
+        }
+    }
+
+    public Cell[] SearchCell(int r, int c)
+    {
+        var list = new List<Cell>();
         
+	    var left = c - 1;
+        var right = c + 1;
+        var top = r - 1;
+        var bottom = r + 1;
+        
+	if (top >= 0)
+        {
+            if (left >= 0 && cubes[r,c].CellState == CellState.None) 
+            { 
+                list.Add(cubes[top, left]); 
+            }
+            list.Add(cubes[top, c]);
+            if (right < m_indexNumX) 
+            { 
+                list.Add(cubes[top, right]); 
+            }
+        }
+        if (left >= 0) 
+        { 
+            list.Add(cubes[r, left]); 
+        }
+        if (right < m_indexNumX) 
+        { 
+            list.Add(cubes[r, right]); 
+        }
+        if (bottom < m_indexNumY)
+        {
+            if (left >= 0) 
+            { 
+                list.Add(cubes[bottom, left]); 
+            }
+            list.Add(cubes[bottom, c]);
+            if (right < m_indexNumX) 
+            { 
+                list.Add(cubes[bottom, right]); 
+            }
+        }
+        
+	return list.ToArray();
     }
 }
