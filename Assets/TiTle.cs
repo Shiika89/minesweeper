8951cd094ed7df8c9a7fd66fccc00f8f3b7ae047
shiika89 <kazuki_plant@yahoo.co.jp>
2021-06-14 20:25:43 +0900

    タイトルとルール説明のシーンを追加

---
+++ Assets/TiTle.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TiTle : MonoBehaviour
+{
+    public void Title()
+    {
+        SceneManager.LoadScene("SMS_StartScene");
+    }
+}
